7f4c5422f09860b3bab229f531be009732ff8fbe
priti-datainsights <priti.aghav@evidyaloka.org>
2026-07-02 11:10:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,12 +190,14 @@ with col4:
 # ==================================================
 # TABS
 # ==================================================
-
-tab1,tab2,tab3 = st.tabs([
-    "📚 Topic Analysis",
-     " Cancelled Sessions",
-    " Offline Sessions"
-])
+tab1 = st.tabs([
+    "📚 Topic Analysis"
+])[0]
+#tab1,tab2,tab3 = st.tabs([
+   # "📚 Topic Analysis",
+    # " Cancelled Sessions",
+    #" Offline Sessions"
+#])
 
 # ==================================================
 # TOPIC ANALYSIS
